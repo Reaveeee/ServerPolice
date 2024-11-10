bc0847ae5a62411ea546ce0473646a054261234b
Reaveeee <reavewt@gmail.com>
2024-11-10 15:14:10 +0100

    add onground check, update movement checks

--- src/main/java/de/reave/serverPolice/Checks/Movement/MovementEvents.java
+++ src/main/java/de/reave/serverPolice/Checks/Movement/MovementEvents.java
@@ -7,57 +7,74 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.util.Vector;
 
-import static de.reave.serverPolice.Utility.Utils.playerDataMap;
-import static de.reave.serverPolice.Utility.Utils.prefixString;
+import static de.reave.serverPolice.Utility.Utils.*;
 
 public class MovementEvents implements Listener {
-    @EventHandler
-    public void onPlayerMove(PlayerMoveEvent event){
-        double hBuffer = .5;
-        double vBuffer = .1;
-        Player player = event.getPlayer();
-        PlayerData playerData = playerDataMap.get(player);
-
-        Vector expectedPositionVector = new Vector(playerData.getLastPosition().getX() + playerData.getLastVelocity().getX(), playerData.getLastPosition().getY() + playerData.getLastVelocity().getY(), playerData.getLastPosition().getZ() + playerData.getLastVelocity().getZ());
-
-        if(player.isOnGround()){
-            playerData.getLastVelocity().setY(0);
-            Location blockLocation = new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY()-.1, player.getLocation().getZ());
-            Block block = blockLocation.getBlock();
-            if(!block.isPassable()){
-                expectedPositionVector.setY(block.getBoundingBox().getMaxY());
-            }
-        }
-
-        Location expectedPosition = new Location(player.getWorld(), expectedPositionVector.getX(), expectedPositionVector.getY(), expectedPositionVector.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());
-
-        if(Math.abs(Math.sqrt(Math.pow(player.getLocation().getX() - expectedPosition.getX(), 2) + Math.pow(player.getLocation().getZ() - expectedPosition.getZ(), 2))) > hBuffer|| Math.abs(player.getLocation().getY() - expectedPosition.getY()) > vBuffer){
-            flagPlayer(player, playerData, expectedPosition);
-        }
-
-        playerData.setLastPosition(player.getLocation());
-        playerData.setLastVelocity(player.getVelocity());
-        playerData.setLastOnGround(player.isOnGround());
-    }
-
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event){
         PlayerData playerData = playerDataMap.get(event.getPlayer());
         playerData.setLastPosition(event.getPlayer().getLocation());
     }
 
-    public void flagPlayer(Player player, PlayerData playerData, Location expectedPosition) {
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+        double hBuffer = .5;
+        double vBuffer = 0;
+        Player player = event.getPlayer();
+        PlayerData playerData = playerDataMap.get(player);
+
+        double acceleration = playerData.getLastVelocity().getY();
+
+        Vector expectedPositionVector = new Vector(playerData.getLastPosition().getX() + playerData.getLastVelocity().getX(), playerData.getLastPosition().getY() + playerData.getLastVelocity().getY(), playerData.getLastPosition().getZ() + playerData.getLastVelocity().getZ());
+
+        expectedPositionVector = fixExpectedPositionVector(expectedPositionVector, player, playerData);
+
+        Location expectedPosition = new Location(player.getWorld(), expectedPositionVector.getX(), expectedPositionVector.getY(), expectedPositionVector.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());
+
+        //Checks
+            //Position
+        if(Math.abs(Math.sqrt(Math.pow(player.getLocation().getX() - expectedPosition.getX(), 2) + Math.pow(player.getLocation().getZ() - expectedPosition.getZ(), 2))) > hBuffer|| Math.abs(player.getLocation().getY() - expectedPosition.getY()) > vBuffer){
+            flagPlayerPosition(player, playerData, expectedPosition, event);
+        }
+            //OnGround
+        if(player.isOnGround() != isPlayerOnGround(player)){
+            flagPlayerSpoofGround(player, playerData);
+        }
+
+        playerData.setLastPosition(player.getLocation());
+        playerData.setLastVelocity(player.getVelocity());
+        playerData.setLastOnGround(isPlayerOnGround(player));
+    }
+
+    public void flagPlayerPosition(Player player, PlayerData playerData, Location expectedPosition, PlayerMoveEvent event) {
         player.getServer().broadcastMessage(prefixString("Unexpected Position | FlagLevel: " + playerData.getFlagLevel() + " | Expected Position: " + expectedPosition.toVector() + " | Position: " + player.getLocation().toVector(), ChatColor.RED));
         playerData.setFlagLevel(playerData.getFlagLevel() + 1);
         if (!expectedPosition.getBlock().isPassable()) {
+            player.teleport(new Location(player.getWorld(), playerData.getLastPosition().getX(), playerData.getLastPosition().getY(), playerData.getLastPosition().getZ(), player.getLocation().getYaw(), player.getLocation().getPitch()));
             return;
         }
         player.teleport(expectedPosition);
+    }
+
+    public void flagPlayerSpoofGround(Player player, PlayerData playerData){
+        player.getServer().broadcastMessage(prefixString("Unexpected OnGround state | FlagLevel: " + playerData.getFlagLevel(), ChatColor.RED));
+        playerData.setFlagLevel(playerData.getFlagLevel() + 1);
+    }
+
+    private Vector fixExpectedPositionVector(Vector vector, Player player, PlayerData playerData){
+        //Case: Player is on ground
+        if(isPlayerOnGround(player)){
+            playerData.getLastVelocity().setY(0);
+            vector.setY(getBlockPlayerStandingOn(player).getBoundingBox().getMaxY());
+        }
+
+        return vector;
     }
 }
--- src/main/java/de/reave/serverPolice/Utility/Utils.java
+++ src/main/java/de/reave/serverPolice/Utility/Utils.java
@@ -2,7 +2,10 @@ package de.reave.serverPolice.Utility;
 
 import de.reave.serverPolice.DataManagement.PlayerData;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 
@@ -10,5 +13,36 @@ public class Utils {
     public static HashMap<Player, PlayerData> playerDataMap = new HashMap<>();
     public static String prefixString(String text, ChatColor chatColor){
         return ChatColor.BLUE + "[SP] " + chatColor + text;
+    }
+
+    public static boolean isPlayerOnGround(Player player){
+        double checkX = 0, checkZ = 0;
+        double piece = (player.getBoundingBox().getWidthX() - .01) / 16;
+        if(player.getVelocity().getX() != 0){
+            return false;
+        }
+        for(int x = -8; x <= 8; x++){
+            checkX = player.getLocation().getX() + x * piece;
+            for(int z = -8; z <= 8; z++){
+                checkZ = player.getLocation().getZ() + z * piece;
+                Location checkLocation = new Location(player.getWorld(), checkX,  player.getLocation().getY() - 0.00001, checkZ);
+                if(!checkLocation.getBlock().isPassable()) return true;
+            }
+        }
+        return false;
+    }
+
+    public static Block getBlockPlayerStandingOn(Player player){
+        double checkX = 0, checkZ = 0;
+        double piece = (player.getBoundingBox().getWidthX() - .01) / 16;
+        for(int x = -8; x <= 8; x++){
+            checkX = player.getLocation().getX() + x * piece;
+            for(int z = -8; z <= 8; z++){
+                checkZ = player.getLocation().getZ() + z * piece;
+                Location checkLocation = new Location(player.getWorld(), checkX,  player.getLocation().getY() - 0.00001, checkZ);
+                if(!checkLocation.getBlock().isPassable()) return checkLocation.getBlock();
+            }
+        }
+        return null;
     }
 }
